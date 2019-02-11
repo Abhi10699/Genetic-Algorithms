5242034d417ace5cfdb527b7d187290f77254fca
Abhi10699 <patelabhi10699@gmail.com>
2019-02-11 13:15:34 +0530

    Changed Word to guess

--- GA_02/main.py
+++ GA_02/main.py
@@ -1,7 +1,7 @@
 from Population import Population
 
 if __name__ == '__main__':
-    p = Population(1000,'123Abc')
+    p = Population(1000,'My Name is abhi')
     while(not p.done):
         p.evaluate()
         p.select()
